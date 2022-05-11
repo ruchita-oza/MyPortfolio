5895284d5b81ad225b6195277c127195bbadeb22
Ruchita Oza <ruchita@atliq.com>
2022-05-11 17:24:51 +0530

    Connected emailform with my email

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -3,8 +3,31 @@ import "./contact.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faGlobeEurope } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import emailjs from "emailjs-com";
 
-function contact() {
+function Contact() {
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    from_email: "",
+    message: "",
+    subject: "",
+  });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send("service_i185qin", "template_9x34bqu", toSend, "EWumzOGPCfEuqBipU")
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+  };
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <section className="contact section" id="contact">
       <div className="container">
@@ -72,64 +95,78 @@ function contact() {
         <h3 className="contact-title">Send Me A Email</h3>
         <h4 className="contact-sub-title">I'M VERY RESPONSIVE TO MAILS</h4>
         {/* <!-- contact form start--> */}
-        <div className="row">
-          <div className="contact-form padd-15">
-            <div className="row">
-              <div className="form-item col-6 padd-15">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Name"
-                  />
+
+        <form onSubmit={onSubmit}>
+          <div className="row">
+            <div className="contact-form padd-15">
+              <div className="row">
+                <div className="form-item col-6 padd-15">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Name"
+                      name="from_name"
+                      value={toSend.from_name}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="form-item col-6 padd-15">
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                      name="from_email"
+                      value={toSend.from_email}
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="form-item col-6 padd-15">
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Email"
-                  />
+              <div className="row">
+                <div className="form-item col-12 padd-15">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Subject"
+                      name="subject"
+                      value={toSend.subject}
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="form-item col-12 padd-15">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Subject"
-                  />
+              <div className="row">
+                <div className="form-item col-12 padd-15">
+                  <div className="form-group">
+                    <textarea
+                      className="form-control"
+                      id=""
+                      name="message"
+                      placeholder="Your message"
+                      value={toSend.message}
+                      onChange={handleChange}
+                    ></textarea>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="form-item col-12 padd-15">
-                <div className="form-group">
-                  <textarea
-                    name=""
-                    className="form-control"
-                    id=""
-                    placeholder="Message"
-                  ></textarea>
+              <div className="row">
+                <div className="form-item col-12 padd-15">
+                  <button className="btn" type="submit">
+                    Send message
+                  </button>
                 </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="form-item col-12 padd-15">
-                <button className="btn" type="submit">
-                  Send message
-                </button>
               </div>
             </div>
           </div>
-        </div>
+        </form>
         {/* <!-- contact form ends --> */}
       </div>
     </section>
   );
 }
 
-export default contact;
+export default Contact;
